12917026183f43d7cc87de0ae9bbe50bd2c94f43
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2024-12-30 14:36:08 +0100

    Adding waiting animation to load data

--- src/Components/PokeList/PokeList.js
+++ src/Components/PokeList/PokeList.js
@@ -4,20 +4,37 @@ import "./PokeList.css"
 
 const PokeList = ({pokemons}) => {
     const [pokemonDetails, setPokemonDetails] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchPokemonDetails = async () => {
-            const details = await Promise.all(
-                pokemons.map(async (pokemon) => {
-                    const response = await fetch(pokemon.url);
-                    return await response.json();
-                })
-            );
-            setPokemonDetails(details);
+            setIsLoading(true);
+            try {
+                const details = await Promise.all(
+                    pokemons.map(async (pokemon) => {
+                        const response = await fetch(pokemon.url);
+                        return await response.json();
+                    })
+                );
+                setPokemonDetails(details);
+            } catch (error) {
+                console.error("Error fetching Pokemon details:", error);
+            } finally {
+                setIsLoading(false);
+            }
         };
 
         fetchPokemonDetails();
     }, [pokemons]);
+
+    if (isLoading) {
+        return (
+            <div className="loading-container">
+                <div className="loading-spinner"></div>
+                <p>waiting to load data...</p>
+            </div>
+        );
+    }
 
     return (
     <div className="pokeList">
